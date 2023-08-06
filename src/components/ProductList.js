465deb11a308814fba058396f70aca149b24c8ec
Bobensky <104023090+Bobensky@users.noreply.github.com>
2023-08-06 11:45:01 +0600

    Update ProductList.js
    Update pagination

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -29,19 +29,61 @@ const ProductList = observer(() => {
         });
     };
 
-    const pages = [];
-    for (let page = 1; page <= catalog.pages; page++) {
-        pages.push(
-        <Pagination.Item
-            key={page}
-            active={page === catalog.page}
-            activeLabel=""
-            onClick={() => handleClick(page)}
-        >
-            {page}
-        </Pagination.Item>
-        );
-    }
+    const PaginationComponent = ({ catalog }) => {
+        const pages = [];
+        const totalPages = 16;
+      
+        if (catalog.page > 1) {
+          pages.push(
+            <Pagination.Item key="prev" onClick={() => handleClick(catalog.page - 1)}>
+              Предыдущая
+            </Pagination.Item>
+          );
+        }
+      
+        if (catalog.page > 2) {
+          pages.push(
+            <Pagination.Item key={1} onClick={() => handleClick(1)}>
+              1
+            </Pagination.Item>
+          );
+      
+          if (catalog.page > 3) {
+            pages.push(<Pagination.Ellipsis key="ellipsis-1" disabled />);
+          }
+        }
+      
+        for (let page = Math.max(1, catalog.page - 1); page <= Math.min(totalPages, catalog.page + 1); page++) {
+          pages.push(
+            <Pagination.Item
+              key={page}
+              active={page === catalog.page}
+              onClick={() => handleClick(page)}
+            >
+              {page}
+            </Pagination.Item>
+          );
+        }
+      
+        if (catalog.page < totalPages - 1) {
+          if (catalog.page < totalPages - 2) {
+            pages.push(<Pagination.Ellipsis key="ellipsis-2" disabled />);
+          }
+      
+          pages.push(
+            <Pagination.Item key={totalPages} onClick={() => handleClick(totalPages)}>
+              {totalPages}
+            </Pagination.Item>
+          );
+        }
+      
+        if (catalog.page < totalPages) {
+          pages.push(
+            <Pagination.Item key="next" onClick={() => handleClick(catalog.page + 1)}>
+              Следующая
+            </Pagination.Item>
+          );
+        }
     
     return (
         <>
