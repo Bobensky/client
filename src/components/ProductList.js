c120a12ebfcecc66c6131242ae0fc2151925d46c
Bobensky <104023090+Bobensky@users.noreply.github.com>
2023-08-07 15:45:27 +0600

    Update ProductList.js
    Fix

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,111 +1,114 @@
+import React from "react";
 import { Row, Pagination } from "react-bootstrap";
 import ProductItem from "./ProductItem.js";
 import { useContext } from "react";
 import { AppContext } from "./AppContext.js";
 import { observer } from "mobx-react-lite";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, createSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 const ProductList = observer(() => {
-  const { catalog } = useContext(AppContext);
-  const navigate = useNavigate();
+    const { catalog } = useContext(AppContext);
+    const navigate = useNavigate();
 
-  const handleClick = (page) => {
-    catalog.page = page;
-    const params = new URLSearchParams();
-    if (catalog.category) params.set("category", catalog.category);
-    if (catalog.brand) params.set("brand", catalog.brand);
-    if (catalog.mehanizm) params.set("mehanizm", catalog.mehanizm);
-    if (catalog.gender) params.set("gender", catalog.gender);
-    if (catalog.shape) params.set("shape", catalog.shape);
-    if (catalog.material) params.set("material", catalog.material);
-    if (catalog.glass) params.set("glass", catalog.glass);
-    if (catalog.strap) params.set("strap", catalog.strap);
-    if (catalog.power) params.set("power", catalog.power);
-    if (catalog.water) params.set("water", catalog.water);
-    if (catalog.page > 1) params.set("page", catalog.page);
-    navigate({
-      pathname: "/shop",
-      search: "?" + params.toString(),
-    });
-  };
+    const handleFirstPage = () => {
+        handleClick(1);
+    };
 
-  const PaginationComponent = ({ catalog, handleClick }) => {
-    const pages = [];
-    const totalPages = 16;
+    const handleLastPage = () => {
+        handleClick(catalog.pages);
+    };
 
-    if (catalog.page > 1) {
-      pages.push(
-        <Pagination.Item key="prev" onClick={() => handleClick(catalog.page - 1)}>
-          Предыдущая
-        </Pagination.Item>
-      );
-    }
+    const handleNextPage = () => {
+        if (catalog.page < catalog.pages) {
+            handleClick(catalog.page + 1);
+        }
+    };
 
-    if (catalog.page > 2) {
-      pages.push(
-        <Pagination.Item key={1} onClick={() => handleClick(1)}>
-          1
-        </Pagination.Item>
-      );
+    const handlePrevPage = () => {
+        if (catalog.page > 1) {
+            handleClick(catalog.page - 1);
+        }
+    };
 
-      if (catalog.page > 3) {
-        pages.push(<Pagination.Ellipsis key="ellipsis-1" disabled />);
-      }
-    }
+    const handleClick = (page) => {
+        catalog.page = page;
+        const params = {
+            category: catalog.category,
+            brand: catalog.brand,
+            mehanizm: catalog.mehanizm,
+            gender: catalog.gender,
+            shape: catalog.shape,
+            material: catalog.material,
+            glass: catalog.glass,
+            strap: catalog.strap,
+            power: catalog.power,
+            water: catalog.water,
+            page: catalog.page,
+        };
+        navigate({
+            pathname: "/shop",
+            search: "?" + createSearchParams(params),
+        });
+    };
 
-    for (let page = Math.max(1, catalog.page - 1); page <= Math.min(totalPages, catalog.page + 1); page++) {
-      pages.push(
-        <Pagination.Item
-          key={page}
-          active={page === catalog.page}
-          onClick={() => handleClick(page)}
-        >
-          {page}
-        </Pagination.Item>
-      );
-    }
+    const renderPageNumbers = () => {
+        const pageNumbers = [];
+        const maxPageButtons = 10; // Максимальное количество кнопок
+        const halfMaxButtons = Math.floor(maxPageButtons / 2);
+        const totalPages = catalog.pages;
 
-    if (catalog.page < totalPages - 1) {
-      if (catalog.page < totalPages - 2) {
-        pages.push(<Pagination.Ellipsis key="ellipsis-2" disabled />);
-      }
+        let startPage = Math.max(1, catalog.page - halfMaxButtons);
+        let endPage = Math.min(totalPages, startPage + maxPageButtons - 1);
 
-      pages.push(
-        <Pagination.Item key={totalPages} onClick={() => handleClick(totalPages)}>
-          {totalPages}
-        </Pagination.Item>
-      );
-    }
+        // Add ellipsis if necessary
+        if (startPage > 1) {
+            pageNumbers.push(
+                <Pagination.Ellipsis key="ellipsis-start" onClick={() => handleClick(startPage - 1)} />
+            );
+        }
 
-    if (catalog.page < totalPages) {
-      pages.push(
-        <Pagination.Item key="next" onClick={() => handleClick(catalog.page + 1)}>
-          Следующая
-        </Pagination.Item>
-      );
-    }
+        // Logic to display page numbers
+        for (let page = startPage; page <= endPage; page++) {
+            pageNumbers.push(
+                <Pagination.Item
+                    key={page}
+                    active={page === catalog.page}
+                    onClick={() => handleClick(page)}
+                >
+                    {page}
+                </Pagination.Item>
+            );
+        }
+
+        if (endPage < totalPages) {
+            pageNumbers.push(
+                <Pagination.Ellipsis key="ellipsis-end" onClick={() => handleClick(endPage + 1)} />
+            );
+        }
+
+        return pageNumbers;
+    };
 
     return (
-      <>
-        <Helmet>
-          {/* ... helmet metadata (unchanged) */}
-        </Helmet>
-        <Row className="mb-3">
-          {catalog.products.length ? (
-            catalog.products.map((item) => (
-              <ProductItem key={item.id} data={item} />
-            ))
-          ) : (
-            <p className="m-3">По вашему запросу ничего не найдено</p>
-          )}
-        </Row>
-        {catalog.products.length > catalog.itemsPerPage && catalog.pages > 1 && <Pagination>{pages}</Pagination>}
-      </>
+        <>
+            <Helmet>
+                {/* Мета-теги */}
+            </Helmet>
+            <Row className="mb-3">
+                {/* Отображение продуктов */}
+            </Row>
+            {catalog.pages > 1 && (
+                <Pagination>
+                    <Pagination.First onClick={handleFirstPage} />
+                    <Pagination.Prev onClick={handlePrevPage} />
+                    {renderPageNumbers()}
+                    <Pagination.Next onClick={handleNextPage} />
+                    <Pagination.Last onClick={handleLastPage} />
+                </Pagination>
+            )}
+        </>
     );
-  };
-
-  return <PaginationComponent catalog={catalog} handleClick={handleClick} />;
 });
 
 export default ProductList;
